9a0cd0102f2ffa6b725a483454658e710e6adbab
tatialveso <tatiialveso@gmail.com>
2022-12-05 19:07:10 -0300

    :sparkles: set router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './components/User/Register';
+import Login from './components/User/Login';
+import Profile from './components/User/Profile';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home /> } />
-        <Route path="/cadastro" element={ <Register /> } />
+        <Route path="/registro" element={ <Register /> } />
         <Route path="/login" element={ <Login /> } />
+        <Route path="/perfil" element={ <Profile /> } />
       </Routes>
     </div>
   );
