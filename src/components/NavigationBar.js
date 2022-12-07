e906d5203af5873f18b83e2129b554445f34d453
tatialveso <tatiialveso@gmail.com>
2022-12-07 20:20:05 -0300

    :sparkles: set new navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,7 +1,8 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function NavigationBar() {
+    const navigate = useNavigate()
     const location = useLocation()
 
     if(location.pathname === "/"
@@ -10,15 +11,26 @@ function NavigationBar() {
             return null
     }
 
+    const handleLogout = () => {
+        localStorage.removeItem("loggedUser");
+        navigate("/");
+    }
+
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand>IronRH</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Link className="nav-link" to="/funcionarios">Página inicial</Link>
-                        <Link className="nav-link" to="/funcionarios/adicionar">Adicionar novo funcionário</Link>
+                    <Nav className="me-auto w-100 d-flex justify-content-between align-items-center">
+                        <div className="d-flex flex-row">
+                            <Link className="nav-link" to="/tarefas">Página inicial</Link>
+                            <Link className="nav-link" to="/tarefas/nova-tarefa">Adicionar nova tarefa</Link>
+                            <Link className="nav-link" to="/perfil">Visualizar perfil</Link>
+                        </div>
+                        <div>
+                            <Button variant="danger" onClick={ handleLogout }>Logout</Button>
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
